3cc1c31c33a8ce7158ed112ba76a302f29697d2b
wetwe <wetwe@14.32.18.23>
2020-08-04 11:11:42 +0900

    첫번째 commit

---
+++ javaEx/src/com/javaex/basic/HellowWorld.java
@@ -0,0 +1,12 @@
+package com.javaex.basic;
+
+/*
+ * 화면에 Hello, World라 출력 해주자
+ */
+public class HellowWorld {
+	// 직접 실행을 위해서는 main 메서드가 있어야 한다.
+	public static void main(String[] args) {
+		// 출력코드
+		System.out.println("Hello, World!");
+	}
+}
